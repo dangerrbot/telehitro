19f270efdc0624f64fe74dae8e79b870733d065e
mentor81 <kavian1381@gmail.com>
2016-02-11 07:37:21 +0330

    Create tosupport.lua

---
+++ plugins/tosupport.lua
@@ -0,0 +1,23 @@
+--- https://github.com/kavian_1381
+--- https://telegram.me/mentor81
+
+do
+
+function run(msg, matches)
+  return " لینک گروه ساپورت ربات : https://telegram.me/joinchat/CT-HhQcRSSc6obnGYVr3oA "
+  end
+return {
+  description = "shows support link", 
+  usage = "!tosupport : Return supports link",
+  patterns = {
+    "^tosupport$",
+    "^/tosupport$",
+    "^!tosupport$",
+    "^>tosupport$",
+  },
+  run = run
+}
+end
+
+--- https://github.com/kavian_1381
+--- https://telegram.me/mentor81
